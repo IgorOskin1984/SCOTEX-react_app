6777db9237d587828414ef440b382a1a92e1732d
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-04-07 19:22:57 +0300

    работа над хедером 1. правильно заполнили хедеры 2. добавил некоторые блоки кода в компоненты; 3. разместили по хедеру

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,13 +2,34 @@ import React from "react";
 import './../../css/App.css'
 import s from './Header.module.css'
 import logo from './../../images/logo.svg'
+import { Button } from "../small_Components/Button/Button";
+import { MenuLink } from "../small_Components/Menu_link/MenuLink";
 
 
 const Header = (props) => {
 	return (
 		<div className={s.header}>
 			<div className={s.container}>
-				хедер
+				<a className={s.logo} href="#">
+					<img src={logo} alt="logo" />
+				</a>
+
+				<nav className={s.menu}>
+					<button className={s.iconMenu} type="button"></button>
+					<ul className={s.menuList}>
+						<MenuLink />
+						<MenuLink />
+						<MenuLink />
+						<MenuLink />
+					</ul>
+				</nav>
+
+				<a className={s.cart} href="3">
+					<span className={s.cartText} >cart</span>
+					<span className={s.cartQuantity}>0</span>
+				</a>
+
+				<Button />
 			</div>
 		</div>
 	)
---
+++ src/Components/small_Components/Menu_link/MenuLink.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import s from './MenuLink.module.css'
+
+export const MenuLink = (props) => {
+	return (
+		<>
+			<li className={s.listItem}>
+				<a className={s.listLink} href="#">Services</a>
+			</li>
+		</>
+	)
+}
