9b8b86ba01c3a423e4c577c86f038945b169507d
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-04-17 14:24:13 +0300

    fit: title css line-height с помощью деления. max-width - непонятно

--- src/Components/Main/Page-About/AboutPage.jsx
+++ src/Components/Main/Page-About/AboutPage.jsx
@@ -4,49 +4,47 @@ import { Button } from "../../small_Components/Button/Button";
 
 const AboutPage = (props) => {
 	console.log(props.peoplsPhotos);
-	return (
-		<>
-			<div className={s.aboutPage}>
-				<div className={s.container}>
-					<div className={s.content}>
-						<h1 className={s.title}>Marketing solutions that grow your business.</h1>
-						<div className={s.text}>
-							<p>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-								suspendisse varius enim in eros elementum tristique.
-								Duis cursus, mi quis viverra ornare, eros dolor.
-							</p>
-						</div>
-						<div className={s.actions}>
-							<Button />
-							<Button />
-						</div>
+	return <>
+		<div className={s.aboutPage}>
+			<div className={s.container}>
+				<div className={s.content}>
+					<h1 className={s.title}>Marketing solutions that grow your business.</h1>
+					<div className={s.text}>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+							suspendisse varius enim in eros elementum tristique.
+							Duis cursus, mi quis viverra ornare, eros dolor.
+						</p>
 					</div>
-
-					<div className={s.images}>
-						<div className={s.imageColumn}>
-							<div className={`${s.imageItem} ${s.item_1}`}>
-								<img src={props.peoplsPhotos[0]} alt="" />
-							</div>
-							<div className={`${s.imageItem} ${s.item_2}`}>
-								<img src={props.peoplsPhotos[1]} alt="" />
-							</div>
-						</div>
-
-						<div className={s.imageColumn}>
-							<div className={`${s.imageItem} ${s.item_3}`}>
-								<img src={props.peoplsPhotos[2]} alt="" />
-							</div>
-							<div className={`${s.imageItem} ${s.item_4}`}>
-								<img src={props.peoplsPhotos[3]} alt="" />
-							</div>
-						</div>
+					<div className={s.actions}>
+						<Button />
+						<Button />
 					</div>
-
 				</div>
+
+				<div className={s.images}>
+					<div className={s.imageColumn}>
+						<div className={`${s.imageItem} ${s.item_1}`}>
+							<img src={props.peoplsPhotos[0]} alt="" />
+						</div>
+						<div className={`${s.imageItem} ${s.item_2}`}>
+							<img src={props.peoplsPhotos[1]} alt="" />
+						</div>
+					</div>
+
+					<div className={s.imageColumn}>
+						<div className={`${s.imageItem} ${s.item_3}`}>
+							<img src={props.peoplsPhotos[2]} alt="" />
+						</div>
+						<div className={`${s.imageItem} ${s.item_4}`}>
+							<img src={props.peoplsPhotos[3]} alt="" />
+						</div>
+					</div>
+				</div>
+
 			</div>
-		</>
-	)
+		</div>
+	</>
 }
 
 export default AboutPage;
