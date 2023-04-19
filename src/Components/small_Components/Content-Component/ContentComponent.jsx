4f2eab9b58a59c77f040a46b9d72351deb1e1fbb
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-04-19 14:22:10 +0300

    feat: заменил а на NavLink to={'/#'} хотел, что бы переключалось на указанный путь сделал это, но оно не переключает на НАЧАЛО указанной страницы. Оставил так. Пока иду дальше

--- src/Components/small_Components/Content-Component/ContentComponent.jsx
+++ src/Components/small_Components/Content-Component/ContentComponent.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import s from './ContentComponent.module.css'
+import { NavLink } from "react-router-dom";
 
 const ContentComponent = (props) => {
-	//debugger
+
+	//неиспользуемая функция
+	const navTo = (navToTaxt) => {
+		switch (navToTaxt) {
+			case 'why choose us':
+				return navToTaxt = '/about'
+			default:
+				return `/${navToTaxt}`;
+		}
+	}
+
 	return <>
-		{props.payload.label && <a a href="#" className={s.label}>{props.payload.label}</a>}
+		{props.payload.label && <NavLink to={'/#'} className={s.label}>{props.payload.label}</NavLink>}
 		{props.payload.label
 			? <h1 className={s.title}>{props.payload.title}</h1>
 			: <h1 className={s.titleWithBefore}>{props.payload.title}</h1>}
