635493bd08984578036bcc799677411bf2cec5ad
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-04-21 14:03:42 +0300

    create ReadyToStart css ReadyToStart myself

--- src/Components/Main/Page-Home/HomePage.jsx
+++ src/Components/Main/Page-Home/HomePage.jsx
@@ -5,9 +5,11 @@ import { AboutPage_1 } from "../Page-About/AboutPage";
 import AdvantagesPageContainer from "../Page-Advantages/Container_AdvantagesPage";
 import TestimonialsContainer from "../Page-Testimonials/Container_Testimonials";
 import BlogPageContaimer from "../Page-Blog/Container_BlogPage";
+import ReadyToStartContainer from "../Page-ReadyToStart/Container_ReadyToStart";
 
 const HomePage = ({ peoplsPhotos, buttonText, buttonColor, aboutPageContent }) => {
 	return <>
+		<ReadyToStartContainer />
 		<AboutPage_1
 			peoplsPhotos={peoplsPhotos}
 			buttonText={buttonText}
---
+++ src/Components/Main/Page-ReadyToStart/ReadyToStart.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import s from './ReadyToStart.module.css'
+import { Button } from "../../small_Components/Button/Button";
+
+const ReadyToStart = (props) => {
+	return (
+		<>
+			<section className={s.readyToStart}>
+				<div className={s.container}>
+					<div className={s.content}>
+						<h2 className={s.title}>Ready to start scaling your business now?</h2>
+						<div className={s.text}>
+							<p>Lorem ipsum dolor sit am consectetur adipiscing varius enim in eros.</p>
+						</div>
+					</div>
+					<div className={s.buttons}>
+						<Button buttonText={props.buttonText1} />
+						<Button buttonText={props.buttonText2} buttonColor={props.buttonColor} />
+					</div>
+				</div>
+			</section>
+		</>
+	)
+}
+export default ReadyToStart;
