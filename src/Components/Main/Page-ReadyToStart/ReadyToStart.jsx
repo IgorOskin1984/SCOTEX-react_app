abc8d166e44f6f5fbfb6c6151bffd03ca881032a
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-04-21 14:05:44 +0300

    feat: css flex: 0 1 50% for  colums

--- src/Components/Main/Page-ReadyToStart/ReadyToStart.jsx
+++ src/Components/Main/Page-ReadyToStart/ReadyToStart.jsx
@@ -7,15 +7,19 @@ const ReadyToStart = (props) => {
 		<>
 			<section className={s.readyToStart}>
 				<div className={s.container}>
-					<div className={s.content}>
-						<h2 className={s.title}>Ready to start scaling your business now?</h2>
-						<div className={s.text}>
-							<p>Lorem ipsum dolor sit am consectetur adipiscing varius enim in eros.</p>
+					<div className={s.colums}>
+						<div className={s.content}>
+							<h2 className={s.title}>Ready to start scaling your business now?</h2>
+							<div className={s.text}>
+								<p>Lorem ipsum dolor sit am consectetur adipiscing varius enim in eros.</p>
+							</div>
 						</div>
 					</div>
-					<div className={s.buttons}>
-						<Button buttonText={props.buttonText1} />
-						<Button buttonText={props.buttonText2} buttonColor={props.buttonColor} />
+					<div className={s.colums}>
+						<div className={s.buttons}>
+							<Button buttonText={props.buttonText1} />
+							<Button buttonText={props.buttonText2} buttonColor={props.buttonColor} />
+						</div>
 					</div>
 				</div>
 			</section>
