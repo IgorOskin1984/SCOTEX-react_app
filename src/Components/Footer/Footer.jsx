6fa0edae65f1bb27ee74d34612c60a5b32a9132b
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-04-22 12:50:04 +0300

    create Footer component черновик лого вынес в commonSateSlice app.js переименовал в app.jsx ??? create footerPageSlice.reducer, заполняю

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,8 +3,77 @@ import s from './Footer.module.css'
 
 const Footer = (props) => {
 	return (
-		<div className={s.footer + ' ' + s.container}>
-			<h1>Footer</h1>
+		<div className={s.footer}>
+
+			<div className={s.mainFooter}>
+				<div className={s.container}>
+					<div className={s.column + ' ' + s.growfy}>
+						<div className={s.growfyImage}>
+							<img src='' alt="growfy" />
+						</div>
+						<div className={s.growfyText}>
+							<p>
+								Growfy is a template highly suitable for modern marketing agencies, digital studios, startups and businesses. The design is made in the dark style, which makes the site memorable and you can easily adapt it to your brand.
+							</p>
+						</div>
+						<div className={s.growfySocial}>
+							<NavLink to={'https://www.linkedin.com/feed/'}>
+								<img src="" alt="linkedin" />
+							</NavLink>
+							<NavLink to={'https://twitter.com/?lang=ru'}>
+								<img src="" alt="twitter" />
+							</NavLink>
+							<NavLink to={'https://www.facebook.com/'}>
+								<img src="" alt="facebook" />
+							</NavLink>
+							<NavLink to={'https://www.youtube.com/'}>
+								<img src="" alt="youtube" />
+							</NavLink>
+							<NavLink to={'https://www.instagram.com/'}>
+								<img src="" alt="instagram" />
+							</NavLink>
+						</div>
+					</div>
+
+					<div className={s.column + ' ' + s.pages}>
+						<div className={s.title}>Pages</div>
+						<nav className={s.list}>
+							<li className={s.item}>
+								<NavLink to={'/home'}>Home</NavLink>
+							</li>
+							<li className={s.item}>
+								<NavLink to={'/about'}>About</NavLink>
+							</li>
+							<li className={s.item}>
+								<NavLink to={'/services'}>Services</NavLink>
+							</li>
+							<li className={s.item}>
+								<NavLink to={'/packages'}>Packages</NavLink>
+							</li>
+							<li className={s.item}>
+								<NavLink to={'/blog'}>Blog</NavLink>
+							</li>
+							<li className={s.item}>
+								<NavLink to={'/contact'}>Contact</NavLink>
+							</li>
+						</nav>
+
+					</div>
+
+					<div className={s.column}>
+
+					</div>
+					<div className={s.column}></div>
+				</div>
+			</div>
+
+			<div className={s.bottomFooter}>
+				<div className={s.container}>
+					<div className={s.copy}>
+						© 2022
+					</div>
+				</div>
+			</div>
 		</div>
 	)
 }
