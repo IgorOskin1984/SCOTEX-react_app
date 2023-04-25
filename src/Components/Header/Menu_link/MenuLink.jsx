0719734aee6427f0e390dadd8d699782d9bff388
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-04-25 11:47:56 +0300

    fix: Burger Menu LiNavLink пришлось отменить hooks чтобы возвращать обычный класс бургеру при переходе по ссылкам меню

--- src/Components/Header/Menu_link/MenuLink.jsx
+++ src/Components/Header/Menu_link/MenuLink.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import s from './MenuLink.module.css'
 import { NavLink } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { showMenuTitlesAC } from "../../../Redux/header-toolkitSlice";
 
 export const LiNavLink = (props) => {
+
+	const dispatch = useDispatch()
+	const onMenuLinkClick = () => {
+		dispatch(showMenuTitlesAC())
+	}
+
 	return (
 		<>
-			<li className={s.listItem + ' ' + props.liClassName}>
-				<NavLink to={`/${props.linkPath}`} className={s.listLink + ' ' + props.className} >{props.title}</NavLink>
+			<li onClick={onMenuLinkClick} className={s.listItem + ' ' + props.liClassName}>
+				<NavLink
+					to={`/${props.linkPath}`}
+					className={s.listLink + ' ' + props.className} >
+					{props.title}
+				</NavLink>
 			</li>
 		</>
 	)
