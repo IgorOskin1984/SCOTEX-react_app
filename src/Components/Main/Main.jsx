eadba681fc19124a51fac063667bc7ea6b66453e
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-04-28 13:40:10 +0300

    Revert "заработало"
    This reverts commit b608ac71ae5ee4067057ff8012f1738c89257366.

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,25 +1,26 @@
-import React, { Suspense, useRef } from "react";
+import React, { Suspense } from "react";
 import './../../css/index.css'
 import s from './Main.module.css'
 import { Navigate, Route, Routes } from "react-router-dom";
 import BlogPageContaimer from "./Page-Blog/Container_BlogPage";
 
-const renderComponent = (componentName, componentRef) => {
+const renderComponent = (componentName) => {
 	const folderName = `Page-${componentName}`
 	const fileName = `Container_${componentName}Page`
 
 	const MyComponent = React.lazy(() => import(`./${folderName}/${fileName}`))
-	return <MyComponent ref={componentRef} />;
+	return <MyComponent />;
 }
 
-const routerCreater = (arr, componentRef) => {
+const routerCreater = (arr) => {
+	//debugger
 	return arr.map((title) => {
-		const path = `/${title}`
+		const headerLink = `/${title}`
 		const componentName = `${title[0].toUpperCase() + title.slice(1)}`;
 		return (
-			<Route key={title} path={path} element={
+			<Route key={title} path={headerLink} element={
 				<Suspense fallback={<div>Загрузка...</div>}>
-					{renderComponent(componentName, componentRef)}
+					{renderComponent(componentName)}
 				</Suspense>
 			} />
 		)
@@ -27,9 +28,6 @@ const routerCreater = (arr, componentRef) => {
 }
 
 const Main = (props) => {
-
-	const componentRef = useRef(null);
-
 	return (
 		<div className={s.mainPage + ' ' + s.container}>
 			<Routes>
@@ -38,7 +36,7 @@ const Main = (props) => {
 						<Navigate to='home' />
 					</Suspense>} />
 
-				{routerCreater(props.headerLinks, componentRef)}
+				{routerCreater(props.headerLinks)}
 
 				<Route path="/blog" element={
 					<Suspense fallback={<div>Загрузка...</div>}>
